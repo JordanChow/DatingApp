46e94336da6d13808e10155671077f2814c72130
Jordan.Chow <Jordan.Chow@peergroup.com>
2021-01-15 11:59:28 -0500

    Adding photo upload functionality

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,35 +1,28 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using API.Data;
-using API.Entities;
+﻿using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading.Tasks;
+using API.Data;
 using API.DTOs;
-using Microsoft.EntityFrameworkCore;
+using API.Entities;
 using API.Interfaces;
-
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 namespace API.Controllers
 {
     public class AccountController : BaseApiController
     {
         private readonly DataContext _context;
         private readonly ITokenService _tokenService;
-
         public AccountController(DataContext context, ITokenService tokenService)
         {
-            _context = context;
             _tokenService = tokenService;
+            _context = context;
         }
-
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
             if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
-
             using var hmac = new HMACSHA512();
             var user = new AppUser
             {
@@ -37,7 +30,6 @@ namespace API.Controllers
                 PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
-
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
             return new UserDto
@@ -46,30 +38,26 @@ namespace API.Controllers
                 Token = _tokenService.CreateToken(user)
             };
         }
-
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.
-                SingleOrDefaultAsync(ECKeyXmlFormat => ECKeyXmlFormat.UserName == loginDto.Username);
+            var user = await _context.Users
+                .Include(p => p.Photos)
+                .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+
             if (user == null) return Unauthorized("Invalid username");
-
             using var hmac = new HMACSHA512(user.PasswordSalt);
-
             var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-            for(int i = 0; i < computedHash.Length; i++)
+            for (int i = 0; i < computedHash.Length; i++)
             {
                 if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
             }
-
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
-
-
         }
 
         private async Task<bool> UserExists(string username)
--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,22 +1,18 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Member } from '../_models/member';
 import { of } from 'rxjs';
 import { map } from 'rxjs/operators';
-
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
-
   constructor(private http: HttpClient) { }
-
   getMembers() {
     if (this.members.length > 0) return of(this.members);
-
     return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
       map(members => {
         this.members = members;
@@ -24,20 +20,25 @@ export class MembersService {
       })
     )
   }
-
   getMember(username: string) {
     const member = this.members.find(x => x.username === username);
     if (member !== undefined) return of(member);
-
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
-
   updateMember(member: Member) {
-    return this.http.put(this.baseUrl + 'users/', member).pipe(
+    return this.http.put(this.baseUrl + 'users', member).pipe(
       map(() => {
         const index = this.members.indexOf(member);
         this.members[index] = member;
       })
     )
+  }
+
+  setMainPhoto(photoId: number) {
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
+  }
+
+  deletePhoto(photoId: number) {
+    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
 }
